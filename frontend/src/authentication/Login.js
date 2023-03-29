3a2bcdec1626477d728c38c8582986aabb2ca311
Gogena01 <gogi2013_gogi2013@abv.bg>
2023-03-29 20:55:10 +0300

    Started building authentication with Context for my project

---
+++ frontend/src/authentication/Login.js
@@ -0,0 +1,63 @@
+import { useState, useEffect, useRef } from 'react';
+
+const Login = () => {
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg('')
+    }, [user, pwd])
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? "errMsg" :
+                "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}> 
+                <label htmlFor="username">Username:</label>
+                <input
+                    type='text'
+                    id='username'
+                    ref={userRef}
+                    autoComplete='off'
+                    onChange={(event) => setUser(event.target.value)}
+                    value={user}
+                    required
+                />
+                <label htmlFor="password">Password:</label>
+                <input
+                    type='password'
+                    id='password'
+                    onChange={(event) => setPwd(event.target.value)}
+                    value={pwd}
+                    required
+                />
+
+                <button>Sign in</button>
+            </form>
+
+
+        </section>
+    )
+}
+
+
+export default Login
